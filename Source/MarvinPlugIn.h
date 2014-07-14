b6f96ff2ce77ce5a7978190986010cfb93ba8346
Christoffer Winterkvist <chris@zenangst.com>
2014-07-14 09:55:53 +0200

    Add MarvinPlugIn class

---
+++ Source/MarvinPlugIn.h
@@ -0,0 +1,13 @@
+//
+//  MarvinPlugIn.h
+//  Marvin
+//
+//  Created by Christoffer Winterkvist on 14/07/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MarvinPlugIn : NSObject
+
+@end
