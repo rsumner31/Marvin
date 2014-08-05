68388c4f46d84f9560db1cc64a7fa7f19550a2e8
Christoffer Winterkvist <chris@zenangst.com>
2014-08-05 21:56:51 +0200

    Fix error

--- Source/Controllers/MarvinManager.h
+++ Source/Controllers/MarvinManager.h
@@ -34,7 +34,6 @@
 - (void)moveToEOLAndInsertTerminatorPlusLF;
 - (void)selectAllWithinBracketsBackward;
 - (void)selectAllWithinBracketsForward;
-- (void)highlightSelection;
 - (void)wrapInBrackets;
 - (void)openCounterpart;
 
