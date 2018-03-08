357cb50b9bbb6dfc5aad9159833b13dfc99074fe
Robert Sumner Jr <rsumner31@gmail.com>
2018-03-07 23:05:55 -0600

    Implement initWithController initialiser

--- Source/MarvinPlugIn.m
+++ Source/MarvinPlugIn.m
@@ -15,4 +15,23 @@
     return @"Marvin";
 }
 
+- (id)initWithPlugInController:(CodaPlugInsController*)aController bundle:(NSBundle*)aBundle
+{
+  return [self initWithController:aController];
+}
+
+- (id)initWithPlugInController:(CodaPlugInsController*)aController
+                  plugInBundle:(NSObject <CodaPlugInBundle> *)plugInBundle
+{
+    return [self initWithController:aController];
+}
+
+- (id)initWithController:(CodaPlugInsController*)inController
+{
+    self = [super init];
+    if (!self) return nil;
+
+    return self;
+}
+
 @end
